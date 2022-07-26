b17e96c953a83d9523c6a350bddfa611514d3d8b
Nayanov Konstantin <8603@mail.ru>
2022-07-26 17:31:43 +0500

    41 Push 6

--- app/src/main/java/ru/netology/nmedia/ui/PushSender.kt
+++ app/src/main/java/ru/netology/nmedia/ui/PushSender.kt
@@ -13,21 +13,15 @@ import ru.netology.R
 import ru.netology.nmedia.dto.Post
 import java.io.FileInputStream
 import kotlin.random.Random
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.messaging.Message
+
 
 class PushSender {
 
    fun sendPushLikeTest(someContext: Context?, post: Post){
         Toast.makeText(someContext, "Like on post (id ${post.id})!", Toast.LENGTH_LONG).show()
-
-
-
-
-            val action = "action"
-            val content = "content"
             val channelId = "remote"
-            val gson = Gson()
-
-
 
        val sccc = (someContext as Context)
        sccc.let {
@@ -39,31 +33,27 @@ class PushSender {
                )
                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                .build()
-
-
            NotificationManagerCompat.from(it)
                 .notify(Random.nextInt(100_000), notification)}
-
+       sendPushLike()
     }
 
     fun sendPushLike(){
-//        val options = FirebaseOptions.builder()
-//            .setCredentials(GoogleCredentials.fromStream(FileInputStream("fcm.json")))
-//            .build()
-//
-//        FirebaseApp.initializeApp(options)
-//
-//        val message = Message.builder()
-//            .putData("action", "LIKE")
-//            .putData("content", """{
-//          "userId": 1,
-//          "userName": "Vasiliy",
-//          "postId": 2,
-//          "postAuthor": "Netology"
-//        }""".trimIndent())
-//            .setToken(token)
-//            .build()
-//
-//        FirebaseMessaging.getInstance().send(message)
+        val options = FirebaseOptions.builder()
+            .setCredentials(GoogleCredentials.fromStream(FileInputStream("fcm.json")))
+            .build()
+        FirebaseApp.initializeApp(options)
+        val message = Message.builder()
+            .putData("action", "LIKE")
+            .putData("content", """{
+          "userId": 1,
+          "userName": "Vasiliy",
+          "postId": 2,
+          "postAuthor": "Netology"
+        }""".trimIndent())
+            .setToken(token)
+            .build()
+
+        FirebaseMessaging.getInstance().send(message)
     }
 }
