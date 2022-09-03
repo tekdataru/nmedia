ca656fd2934c062b5e1431f77ae16caf043f110b
Nayanov Konstantin <8603@mail.ru>
2022-09-03 13:50:12 +0500

    56 Retrofit 1

--- app/src/main/java/ru/netology/nmedia/data/impl/PostRepositoryHttpImpl.kt
+++ app/src/main/java/ru/netology/nmedia/data/impl/PostRepositoryHttpImpl.kt
@@ -9,6 +9,8 @@ import com.google.gson.reflect.TypeToken
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
+import ru.netology.nmedia.api.PostsApi
+import ru.netology.nmedia.api.PostsApiService
 import ru.netology.nmedia.data.EMPTY_POST
 import ru.netology.nmedia.data.PostRepository
 import ru.netology.nmedia.dto.Post
@@ -19,23 +21,10 @@ import java.lang.RuntimeException
 import java.util.concurrent.TimeUnit
 
 class PostRepositoryHttpImpl : PostRepository {
-    private val client = OkHttpClient.Builder()
-        .connectTimeout(30, TimeUnit.SECONDS)
-        .build()
-    private val gson = Gson()
-    private val typeToken = object : TypeToken<List<Post>>() {}
-    //private val typeTokenPost = object : TypeToken<Post::class.java>(){}
-
-    companion object {
-        private const val BASE_URL = "http://10.0.2.2:9999"
-        private val jsonType = "application/json".toMediaType()
-    }
 
 
     override fun getAll(): List<Post> {
-        val request: Request = Request.Builder()
-            .url("${BASE_URL}/api/slow/posts")
-            .build()
+
 
         return client.newCall(request)
             .execute()
@@ -44,23 +33,23 @@ class PostRepositoryHttpImpl : PostRepository {
     }
 
     override fun getAllAsync(callback: PostRepository.GetAllCallback) {
-        val request: Request = Request.Builder()
-            .url("${BASE_URL}/api/slow/posts")
-            .build()
 
-        client.newCall(request)
-            .enqueue(object : Callback {
-                override fun onResponse(call: Call, response: Response) {
-                    val body = response.body?.string() ?: throw RuntimeException("body is null")
-                    try {
-                        callback.onSuccess(gson.fromJson(body, typeToken.type))
-                    } catch (e: Exception) {
-                        callback.onError(e)
+        PostsApi.retrofitService.getAll()
+            .enqueue(object : retrofit2.Callback<List<Post>> {
+                override fun onResponse(
+                    call: retrofit2.Call<List<Post>>,
+                    response: retrofit2.Response<List<Post>>
+                ) {
+                    if (!response.isSuccessful) {
+                        callback.onError(RuntimeException(response.message()))
+                        return
                     }
+
+                    callback.onSuccess(response.body() ?: throw RuntimeException("body is null"))
                 }
 
-                override fun onFailure(call: Call, e: IOException) {
-                    callback.onError(e)
+                override fun onFailure(call: retrofit2.Call<List<Post>>, t: Throwable) {
+                    TODO("Not yet implemented")
                 }
             })
     }
@@ -158,52 +147,40 @@ class PostRepositoryHttpImpl : PostRepository {
     }
 
     override fun save(post: Post) {
-        //До асинхронов+
-        val request: Request = Request.Builder()
-            .post(gson.toJson(post).toRequestBody(jsonType))
-            .url("${BASE_URL}/api/slow/posts")
-            .build()
-
-        client.newCall(request)
+        val result = PostsApi.retrofitService.save(post)
             .execute()
-            .close()
-        //До асинхронов-
+        if (!result.isSuccessful) {
+            error(result.message())
+        }
+
+        result.body() ?: error("Body is null")
 
     }
 
-    override fun saveAsync(post: Post, callback: PostRepository.CallbackWithNoParameters) {
+    /*  override fun saveAsync(post: Post, callback: PostRepository.CallbackWithNoParameters) {
 
-        val request: Request = Request.Builder()
-            .post(gson.toJson(post).toRequestBody(jsonType))
-            .url("${BASE_URL}/api/slow/posts")
-            .build()
 
-        client.newCall(request)
-            .enqueue(object : Callback {
-                override fun onResponse(call: Call, response: Response) {
-                    println("!!!!!! Response on save post in PostRepositoryImpl")
-                    callback.onSuccess()
+          client.newCall(request)
+              .enqueue(object : Callback {
+                  override fun onResponse(call: Call, response: Response) {
+                      println("!!!!!! Response on save post in PostRepositoryImpl")
+                      callback.onSuccess()
 
-                }
+                  }
 
-                override fun onFailure(call: Call, e: IOException) {
-                    // Toast.makeText(this, "some error on save in repositoryImpl", Toast.LENGTH_LONG).show()
-                    callback.onError(e)
-                    println("!!!!!! error on save post in PostRepositoryImpl")
-                }
-            }
-            )
-    }
+                  override fun onFailure(call: Call, e: IOException) {
+                      // Toast.makeText(this, "some error on save in repositoryImpl", Toast.LENGTH_LONG).show()
+                      callback.onError(e)
+                      println("!!!!!! error on save post in PostRepositoryImpl")
+                  }
+              }
+              )
+      }*/
 
     override fun removeById(id: Long) {
-        val request: Request = Request.Builder()
-            .delete()
-            .url("${BASE_URL}/api/slow/posts/$id")
-            .build()
-
-        client.newCall(request)
+        val response = PostsApi.retrofitService.delete(id)
             .execute()
-            .close()
+
     }
 
     override fun removeByIdAsync(id: Long, callback: PostRepository.CallbackWithNoParameters) {
